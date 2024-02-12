612d1fd9222b58de85c0f053e659932170724dfe
James Michael <jomichael1994@gmail.com>
2024-02-12 19:39:18 +0000

    set player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,22 @@
-export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
-}
+import { useState, useRef } from 'react';
+
+const Player = () => {
+	const playerNameRef = useRef();
+	const [playerName, setPlayerName] = useState(null);
+
+	return (
+		<section id="player">
+			<h2>Welcome {playerName ?? 'unknown entity'}</h2>
+			<p>
+				<input ref={playerNameRef} type="text" />
+				<button
+					onClick={() => setPlayerName(playerNameRef.current.value)}
+				>
+					Set Name
+				</button>
+			</p>
+		</section>
+	);
+};
+
+export default Player;
